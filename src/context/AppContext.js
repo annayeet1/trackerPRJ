ba36fda3b6efa4a2a7cd77942ec43aba0a7abae1
annayeet1 <138150476+annayeet1@users.noreply.github.com>
2026-01-29 14:33:30 -0600

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
 import { getTodayDate, archiveData } from '../utils/dateUtils';
 
 const AppContext = createContext();
